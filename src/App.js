13865eb3e7655278d2db86059f44fd2b009e1afe
Jordan Duran <jordanduran@static-192-165-1-7.cust.crystone.se>
2020-08-07 16:25:12 -0400

    Import MainNavigation

--- src/App.js
+++ src/App.js
@@ -8,19 +8,23 @@ import {
 
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
+import MainNavigation from './shared/Navigation/MainNavigation';
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route exact path='/'>
-          <Users />
-        </Route>
-        <Route exact path='/places/new'>
-          <NewPlace />
-        </Route>
-        <Redirect to='/' />
-      </Switch>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route exact path='/'>
+            <Users />
+          </Route>
+          <Route exact path='/places/new'>
+            <NewPlace />
+          </Route>
+          <Redirect to='/' />
+        </Switch>
+      </main>
     </Router>
   );
 };
