887e60e8d82a64e30356b639dbd79d062d1702e2
Jordan Duran <jordanduran@static-192-165-1-7.cust.crystone.se>
2020-08-07 15:19:47 -0400

    Users dummy data

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
+import UsersList from '../components/UsersList';
+
 const Users = () => {
-  return <h1>Users Page</h1>;
+  const USERS = [
+    {
+      id: 'u1',
+      name: 'Jordan Duran',
+      image:
+        'https://duckduckgo.com/?q=image+pexels&atb=v224-1&iax=images&ia=images&iai=https%3A%2F%2Fimages.pexels.com%2Fphotos%2F1133505%2Fpexels-photo-1133505.jpeg%3Fcs%3Dsrgb%26dl%3Dasphalt-blue-clouds-1133505.jpg%26fm%3Djpg',
+      places: 3,
+    },
+  ];
+
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
