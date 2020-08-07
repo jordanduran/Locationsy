e2d2142e310ec5e7e6178b6d08711edcd71a8e88
Jordan Duran <jordanduran@static-192-165-1-7.cust.crystone.se>
2020-08-07 15:03:51 -0400

    react-router setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
 
-function App() {
+import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace';
+
+const App = () => {
   return (
-    <div className='App'>
-      <h1>App</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <Users />
+        </Route>
+        <Route exact path='/places/new'>
+          <NewPlace />
+        </Route>
+        <Redirect to='/' />
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
